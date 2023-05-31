07921ffd90efb36f6aa501a0a907096d166b8ac0
Kaciade <valerypolyakov60@gmail.com>
2023-05-31 16:34:25 +0200

    В процессе добавления возможности вывода информации в терминал

--- Task.py
+++ Task.py
@@ -1,14 +1,11 @@
-# surname = ["Петров","Иванов","Сергеев","Поляков","Кулик","Зверев","Максимов"]
-# name = ["Пётр","Иван","Сергей","Валерий","Роман","Владислав","Максим"]
-# phone_Num = ["891","892","893","894","895","896","897"]
-# discription = ["рабочий","домашний","рабочий","личный","личный","домашний","домашний"]
-
 def get_Info():
     info = []
     surname = input("Введите фамилию: ")
     info.append(surname)
     name = input("Введите имя: ")
     info.append(name)
+    patronymic = input("Введите отчество: ")
+    info.append(patronymic)
     phone_Num = ""
     valid = False
     while not valid:
@@ -22,21 +19,46 @@ def get_Info():
         except:
             print("Номер телефона должен состоять только из цифр.")
     info.append(phone_Num)
-    description = input("Введите описание: ")
-    info.append(description)
     return info
 
 def writing_Txt_File(info):
     file = "Phonebook.txt"
     with open(file, "a", encoding = "utf-8") as data:
         data.write(
-            f"Фамилия: {info[0]}\nИмя: {info[1]}\nНомер телефона: {info[2]}\nОписание: {info[3]}\n\n")
+            f"Фамилия: {info[0]}\nИмя: {info[1]}\nИмя: {info[2]}\nНомер телефона: {info[3]}\n\n")
         
 def from_File(file):
     with open(file, "r", encoding="utf-8") as data:
         dictionary = data.read()
     return dictionary
 
+def view():
+    print(from_File("Phonebook.txt"))
+
+
+def record_info():
+    info = get_Info()
+    writing_Txt_File(info)
+
+def choice():
+    flag = input(
+        "Для продолжения работы нажмите \"да\", или любой символ для завершения работы...")
+    while (flag.lower() == "да"):
+        path = "Phonebook.csv"
+        valid = exists(path)
+        if not valid:
+            creating()
+        choice_action = input(
+            'Введите \'да\', если хотите записать новые данные, и любой другой символ, если хотите просмотреть справочник в консоли: ')
+        if choice_action.lower() == 'да':
+            record_info()
+        else:
+            view()
+        flag = input(
+            'Для продолжения работы нажмите \'да\', или любой символ для завершения работы... ')
+    print('Программа завершена.')
+
 info = get_Info()
 writing_Txt_File(info)
-from_File("Phonebook.txt")
+search = input("\nПоиск: ")
+from_File(search)
